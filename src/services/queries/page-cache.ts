708f2a0aaab1ed9c6f2286cea3b86eb705f6d695
SuphawitCE <b4nknaja@hotmail.com>
2022-12-29 17:58:58 +0700

    feat(fn): Page key generation 'pageCacheKey' function

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,10 +1,11 @@
 import { client } from '$services/redis';
+import { pageCacheKey } from '$services/keys';
 
 const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup'];
 
 export const getCachedPage = (route: string) => {
 	if (cacheRoutes.includes(route)) {
-		return client.get(`pagecache#${route}`);
+		return client.get(pageCacheKey(route));
 	}
 
 	return null;
@@ -14,6 +15,6 @@ export const setCachedPage = (route: string, page: string) => {
 	const options = { EX: 2 };
 
 	if (cacheRoutes.includes(route)) {
-		return client.set(`pagecache#${route}`, page, options);
+		return client.set(pageCacheKey(route), page, options);
 	}
 };
